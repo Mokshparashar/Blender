aee2456dddd0b8e36a40c5d07f31c043a079154b
Moksh parashar <parasharm239@gmail.com>
2023-10-08 12:37:16 +0530

    structure finalize

--- src/components/adminDashboard/Navbar.jsx
+++ src/components/adminDashboard/Navbar.jsx
@@ -1,11 +1,14 @@
-import navbarClasses from '../../style/AdminDashboard/navbar.module.css'
-
+import navbarClasses from "../../style/AdminDashboard/navbar.module.css";
+import { FaXTwitter } from "react-icons/fa6";
 const Navbar = () => {
   return (
     <nav>
-      
+      <div className="logo-box">
+        <FaXTwitter />
+      </div>
+      <div className="profile-box"></div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
