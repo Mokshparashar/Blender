4131b119839d1175b3fb50937f83c692d54dd134
Moksh parashar <parasharm239@gmail.com>
2023-10-09 19:05:02 -0700

    sidebar fixed

--- src/components/adminDashboard/Sidebar.jsx
+++ src/components/adminDashboard/Sidebar.jsx
@@ -2,14 +2,27 @@ import { sidebarActions } from "../../store/adminDashStore";
 import { useSelector, useDispatch } from "react-redux";
 import sidebarClasses from "../../style/AdminDashboard/sidebar.module.css";
 import { FaXTwitter } from "react-icons/fa6";
+import { RxCross1 } from "react-icons/rx";
 const Sidebar = () => {
   const dispatch = useDispatch();
   const isSidebarOpen = useSelector((state) => state.isSidebarOpen);
+
+  const handleSidebarClose = () => {
+    dispatch(sidebarActions.sidebarClose());
+  };
   return (
-    !isSidebarOpen && (
+    isSidebarOpen && (
       <div className={sidebarClasses.sidebar}>
         <div className={sidebarClasses.sidebarLogoBox}>
-          <FaXTwitter />
+          <div className={sidebarClasses.sidebarLogo}>
+            <FaXTwitter />
+          </div>
+          <div
+            className={sidebarClasses.sidebarCancelBtn}
+            onClick={handleSidebarClose}
+          >
+            <RxCross1 />
+          </div>
         </div>
       </div>
     )
